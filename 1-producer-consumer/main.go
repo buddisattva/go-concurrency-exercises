318c64a0da0a2d3ec21207fc6afccf0de8cd8fc7
Wolfgang Xu <wolfgang_xu@paradise-soft.com.tw>
2020-10-19 15:28:30 +0800

    feat: implement an unstable version by channel/select

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////
 //
-// Given is a producer-consumer szenario, where a producer reads in
+// Given is a producer-consumer scenario, where a producer reads in
 // tweets from a mockstream and a consumer is processing the
 // data. Your task is to change the code so that the producer as well
 // as the consumer can run concurrently
@@ -13,24 +13,55 @@ import (
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
+func producer(stream Stream, tweets *[]*Tweet, ch chan struct{}) {
 	for {
-		tweet, err := stream.Next()
-		if err == ErrEOF {
-			return tweets
-		}
+		select {
+		default:
+			go func() {
+				defer func() {
+					err := recover()
+					if err == nil {
+						return
+					}
 
-		tweets = append(tweets, tweet)
+					value, ok := err.(error)
+					if !ok {
+						return
+					}
+
+					if value.Error() == "runtime error: index out of range" {
+						return // skip this exception
+					}
+
+					panic(recover()) // problems happened
+				}()
+
+				tweet, err := stream.Next()
+				if err == ErrEOF {
+					ch <- *new(struct{}) // tell the channel, this loop should be stopped
+				} else {
+					*tweets = append(*tweets, tweet)
+				}
+			}()
+		case <-ch:
+			return
+		}
 	}
 }
 
-func consumer(tweets []*Tweet) {
+func consumer(tweets []*Tweet, ch chan string) {
 	for _, t := range tweets {
-		if t.IsTalkingAboutGo() {
-			fmt.Println(t.Username, "\ttweets about golang")
-		} else {
-			fmt.Println(t.Username, "\tdoes not tweet about golang")
-		}
+		cloned := new(Tweet)
+		*cloned = *t
+		go consumeTweet(*cloned, ch)
+	}
+}
+
+func consumeTweet(t Tweet, ch chan string) {
+	if t.IsTalkingAboutGo() {
+		ch <- t.Username + "\ttweets about golang"
+	} else {
+		ch <- t.Username + "\tdoes not tweet about golang"
 	}
 }
 
@@ -39,10 +70,27 @@ func main() {
 	stream := GetMockStream()
 
 	// Producer
-	tweets := producer(stream)
+	tweets := new([]*Tweet)
+	chEOF := make(chan struct{})
+	producer(stream, tweets, chEOF)
 
 	// Consumer
-	consumer(tweets)
+	ch := make(chan string, len(*tweets))
+	consumer(*tweets, ch)
+
+	for {
+		if cap(ch) == len(*tweets) {
+			break
+		}
+		time.Sleep(time.Nanosecond)
+	}
+
+	for msg := range ch {
+		fmt.Println(msg)
+		if len(ch) < 1 {
+			break
+		}
+	}
 
 	fmt.Printf("Process took %s\n", time.Since(start))
 }
