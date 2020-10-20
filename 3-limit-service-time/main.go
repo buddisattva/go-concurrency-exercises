53b23cf7bcc40df56682452e194594b223b20611
Wolfgang Xu <wolfgang_xu@paradise-soft.com.tw>
2020-10-20 11:49:51 +0800

    feat: accumulate time used of users and restrict free users

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,10 @@
 
 package main
 
+import (
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -18,11 +22,34 @@ type User struct {
 	TimeUsed  int64 // in seconds
 }
 
+var timestamps = make(map[*User]chan time.Time)
+
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
+	if timestamps[u] == nil {
+		timestamps[u] = make(chan time.Time, 1)
+	}
+
+	accumulateTimeUsed(u)
 	process()
+	accumulateTimeUsed(u)
+
+	if !u.IsPremium && u.TimeUsed >= 10 {
+		return false
+	}
 	return true
+}
+
+func accumulateTimeUsed(u *User) {
+	select {
+	case t := <-timestamps[u]:
+		u.TimeUsed += int64(time.Since(t).Seconds())
+	default:
+		u.TimeUsed = 0
+	}
+	timestamps[u] <- time.Now()
+	//fmt.Printf("UserID: %d || %d sec elapsed\n", u.ID, u.TimeUsed)
 }
 
 func main() {
